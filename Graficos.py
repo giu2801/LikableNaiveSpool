390d72a4ee1a709fde380993a390d62a80e6bef8
Giuliana Pola <giulianapola@usp.br>
2020-06-08 09:14:13 +0000

    080620 06:14 Passo 3 pronto

--- Graficos.py
+++ Graficos.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import numpy as np
-
 def simples(o,aa,num):
+  import matplotlib.pyplot as plt
+  import numpy as np
   # Fixing random state for reproducibility
   np.random.seed(19680801)
   plt.rcdefaults()
@@ -23,3 +21,39 @@ def simples(o,aa,num):
   #plt.show()
 
   fig.savefig('Passo 3/Catalase de '+o+'.png', transparent=False, dpi=80, bbox_inches="tight")
+
+def agrupado(aa,num):
+  import matplotlib.pyplot as plt
+  import matplotlib.patches as mpatches
+  import numpy
+
+  # data
+  organismos=list(num.keys())
+  x = numpy.arange(len(num[organismos[0]]))
+
+  #plot data
+  #use zorder to put bars in front of grid
+  bar_width=0.15
+  i=0
+  colors=['green','red','orange','blue']
+  for o in organismos:
+    plt.bar(x + bar_width*i, num[o], width=bar_width, color=colors[i], zorder=2)
+    i=i+1
+
+  #labels
+  #adjust x until it is centered
+  plt.xticks(x + bar_width*2, aa)
+  plt.title('Catalase de 4 organismos')
+  plt.xlabel('Aminoácido')
+  plt.ylabel('Número de ocorrências')
+
+  #legend
+  green_patch = mpatches.Patch(color='green', label=organismos[0])
+  red_patch=mpatches.Patch(color='red', label=organismos[1])
+  orange_patch = mpatches.Patch(color='orange', label=organismos[2])
+  blue_patch = mpatches.Patch(color='blue', label=organismos[3])
+  plt.legend(handles=[green_patch, red_patch,orange_patch, blue_patch],loc='upper center', bbox_to_anchor=(0.5, -0.05),fancybox=True, shadow=True, ncol=5)
+
+  #grid
+  plt.grid(axis='y')
+  plt.savefig('Passo 3/Catalase de 4 organismos.png', transparent=False, dpi=80, bbox_inches="tight")
--- main.py
+++ main.py
@@ -6,11 +6,13 @@ with open('Aminoacidos.txt','r') as l:
 
 organismos=['Camundongo','Cão','Gado-doméstico','Humano']
 dna=dict()
+completo=""
 
 for o in organismos:
   with open('DNA/'+o+'.fasta','r') as l:
     l.readline()
     dna[o]=l.read()
+    completo=completo+dna[o]
 
 import re
 import Graficos
@@ -19,10 +21,11 @@ with open('Passo 3/Catalase-aminoacidos.txt','w') as l:
   for o in organismos:
     l.write(o.upper())
     l.write('\n%20s|%18s|%21s|%23s'%('Código do aminoácido','Nome do aminoácido','Número de ocorrências','Posições de ocorrências'))
-    aa3=dict()
+    aa3=[]
     aa2=[]
     num=[]
     lista=''
+    códigos=[]
     for a in aa:
       if dna[o].count(a)>0:
         aa2.append(aa[a])
@@ -33,7 +36,10 @@ with open('Passo 3/Catalase-aminoacidos.txt','w') as l:
           else:
             lista=lista+','+str(b.start())
         l.write('\n%20s|%18s|%21s|%23s'%(a,aa[a],dna[o].count(a),lista))
-        aa3[a]=dna[o].count(a)
+      if completo.count(a)>0:
+        códigos.append(a)
+        aa3.append(dna[o].count(a))
+    #Graficos.simples(o,aa2,num)
     organismos2[o]=aa3
-    Graficos.simples(o,aa2,num)
     l.write('\n\n')
+Graficos.agrupado(códigos,organismos2)
